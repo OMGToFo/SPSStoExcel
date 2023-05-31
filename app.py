ec159fb65f1c3906910cde4057161c03424aaaf5
OMGToFo <93513240+OMGToFo@users.noreply.github.com>
2023-05-31 09:41:43 +0200

    Update 2023.05.31 mit erstes Crosstabeling

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import pyreadstat
 import tempfile
 from io import BytesIO
 
+from io import StringIO 
+
+import numpy as np
+
 #Um den Datensatz zu analysieren:
 from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
@@ -120,7 +124,7 @@ if file is not None:
                 processed_data = output.getvalue()
                 return processed_data
 
-
+            st.write("")
             df_xlsx = to_excel(rawData)
             st.download_button(label='📥 Export Rawdata to Excel?',
                                data=df_xlsx,
@@ -139,17 +143,53 @@ if file is not None:
             if statisticalTestsRawData:
  
                my_korrelationsVariablenSelect = st.multiselect("Choose variables for tests",rawData.columns.tolist())
-               df_statistischeTestrawData = rawData[my_korrelationsVariablenSelect]       
-   
-               
+               df_statistischeTestrawData = rawData[my_korrelationsVariablenSelect]
+
+               st.write("")
+
+               if st.checkbox("Show simple Chart?"):
+               #bygga in en chartmodul här?
+                   #st.write(df_statistischeTestrawData)
+                   averages = df_statistischeTestrawData[my_korrelationsVariablenSelect].mean()
+                   #st.write(averages)
+
+                   chart_type = st.radio('Select chart type', ('Horizontal Bar Chart', 'Vertical Line Chart'))
+
+                   st.set_option('deprecation.showPyplotGlobalUse', False) #wegen Fehlermeldung
+
+
+
+                   chart_data = pd.DataFrame({'Columns': my_korrelationsVariablenSelect, 'Averages': averages})
+                   if chart_type == 'Horizontal Bar Chart':
+                       plt.barh(chart_data['Columns'], chart_data['Averages'])
+                       plt.xlabel('Columns')
+                       plt.ylabel('Average Value')
+                       plt.title('Average Values of Selected Columns')
+                       for i, avg in enumerate(averages):
+                           plt.text(avg, i, f'{avg:.2f}', ha='left', va='center')
+                       st.pyplot()
+
+                   elif chart_type == 'Vertical Line Chart':
+                    #vertikal line chart
+                       plt.plot(chart_data['Averages'], chart_data['Columns'], marker='o')
+                       plt.xlabel('Columns')
+                       plt.ylabel('Average Value')
+                       plt.title('Average Values of Selected Columns')
+                       plt.xticks(rotation=45)
+                       st.pyplot()
+
+               st.write("")
+
                if len(my_korrelationsVariablenSelect)>0:
   
                    if st.checkbox("Show descriptive Info?"):
                        st.write(df_statistischeTestrawData.describe())
                        st.write("")
                        st.write(df_statistischeTestrawData.info())
-                       st.write("")                
-                   
+                       st.write("")
+
+                   st.write("")
+
                    if st.checkbox("Show Pearson correlation coefficients?"):                 
                      # Compute Pearson correlation coefficient for the features in our data set.
                         # The correlation method in pandas, it has the Pearson correlation set as default.
@@ -170,7 +210,8 @@ if file is not None:
                     plt.title('Correlation Heatmap',fontsize=8)
                     st.write(fig)
                    """
-                   
+
+                   st.write("")
                    
                    if st.button("Show Profile-Reporting?"):
 
@@ -188,6 +229,7 @@ if file is not None:
 
 
         st.write("")
+        st.write('---')
         st.write("")
         
         
@@ -230,12 +272,7 @@ if file is not None:
                     if row['Column Name'] in labelledData.columns:
                         labelledData.rename(columns={row['Column Name']: row['VariableLabelUnique']}, inplace=True)  
     
-            
 
-            
-
-                
-                
             st.write(labelledData)
     
             if st.checkbox("Show Column Data Types of labelled data?"):
@@ -278,7 +315,10 @@ if file is not None:
                if len(my_korrelationsVariablenSelect)>1:
                     
                 if st.checkbox("Show descriptive Info of labeled Data?"):
-                       st.write(df_statistischeTestLabeledData.describe())
+                       #st.write(df_statistischeTestLabeledData.describe())
+
+                       st.write(df_statistischeTestLabeledData.describe(include=np.object))
+
                        st.write("")
                        st.write(df_statistischeTestLabeledData.info())
                        st.write("")                
@@ -302,14 +342,214 @@ if file is not None:
                     
                     export=profile.to_html()
                     st.download_button(label="Download Profile Report", data=export, file_name='report.html')
-    
-        
-        
-        
- ########################################## Metadata  ##############################################################################################################################   
+
+
+
+
+        ########################################## Combined Data -   merge raw (categoriel data) and labelled data  ##############################################################################################################################
+        st.write("")
+        st.write('---')
+        st.write("")
+
+        if len(rawData)>1 and len(labelledData)>1:
+
+
+            MergedDataExpander = st.expander("Merge categorical and numerical variables to a new dataset?") ############################
+            with MergedDataExpander:
+
+                # Load the data frames
+                df1 = rawData
+                df2 = labelledData
+
+                # Get the column names
+                numeric_cols = df1.columns.tolist()
+                categorical_cols = df2.columns.tolist()
+
+                # Add prefixes to column names to avoid conflicts
+                df1_prefixed = df1.add_prefix('numeric_')
+                df2_prefixed = df2.add_prefix('categorical_')
+
+                # Let the user select columns to merge
+                st.write("")
+                selected_numeric_cols = st.multiselect('Select columns with numeric values (brand awareness 0-100, sympathy 1-7,...) :', numeric_cols)
+                st.write("")
+                selected_categorical_cols = st.multiselect('Select columns with categories (men/women, old/young..):', categorical_cols)
+                st.write("")
+                # Merge the selected columns
+                merged_df = pd.DataFrame()
+
+                # Merge numeric columns
+
+                # Merge categorical columns
+                for col in selected_categorical_cols:
+                    merged_df[col] = df2_prefixed['categorical_' + col]
+
+
+
+                for col in selected_numeric_cols:
+                    merged_df[col] = df1_prefixed['numeric_' + col]
+
+
+                st.write("## Combined dataset")
+                if rename_columns:
+                    st.info("Datafile with renamed columns")
+                # Display the merged data frame
+                st.write("Table with selected columns (merged_df):")
+                st.dataframe(merged_df)
+
+
+                if st.checkbox("Show labelling/unique values?"):
+                    anzahlVariablen = len(selected_numeric_cols) + len(selected_categorical_cols)
+                    st.write("Anzahl Variablen: ",anzahlVariablen)
+
+                    st.write("selected_numeric_cols:", selected_numeric_cols)
+                    st.write("selected_categorical_cols:", selected_categorical_cols)
+
+                    st.markdown("#### Ausgewähle Variablen und die jeweilige Auswahl an Ausprägungen:")
+                    st.info("Zeilen die None enthalten werden gelöscht")
+                    cols = st.columns(anzahlVariablen)
+                    
+                    VariablenKolumnenAuswahl = merged_df.columns.values.tolist()
+                    #st.write("VariablenKolumnenAuswahl: ",VariablenKolumnenAuswahl)
+
+
+
+                    dict_of_merged_df= {} # initialize empty dictionary
+
+                    merged_df = merged_df.dropna(axis = 0, how ='any') 
+
+                    for i in range(anzahlVariablen):
+                        col = cols[i%anzahlVariablen]
+                        AuspraegungenAuswahlEinzeln = col.multiselect(f"" + VariablenKolumnenAuswahl[i],
+                        options=merged_df[VariablenKolumnenAuswahl[i]].unique(),
+                        default = merged_df[VariablenKolumnenAuswahl[i]].unique(),
+                        key=i
+                        )
+
+                    AnzahlKategorischeVariablen = len(selected_categorical_cols)
+                    for k in range(AnzahlKategorischeVariablen):
+                        KategorienListe = merged_df[VariablenKolumnenAuswahl[k]].unique()
+
+                        #st.write("KategorienListe: ",KategorienListe)
+
+                        #st.write("i: ",i)
+                        #col.text_input('Ausprägung', key=i)
+                        #Dataframe je Variable mit den Ausprägungen bildenm ev können wir hier dann ein Labeling machen?
+                        
+                        dict_of_merged_df["merged_df_{}".format(i)] = merged_df[VariablenKolumnenAuswahl[i]].unique()
+                        #col.write(dict_of_merged_df["merged_df_{}".format(i)])
+                        
+
+                if st.checkbox("Show descriptive Infos?"):
+                    st.write(merged_df.describe())
+                    st.write("")
+
+
+                #st.write(merged_df.dtypes)
+
+                if st.checkbox("Create cross-tabulations?"):
+                    st.title("Cross Tables with Average Values")
+
+                    # Create multiselect widgets for object and float variables
+                    selected_object_vars = selected_categorical_cols
+                    selected_float_vars = selected_numeric_cols
+
+                    # Generate cross table with average values
+                    if selected_object_vars and selected_float_vars:
+
+                        _="""
+                        cross_table = pd.pivot_table(merged_df, values=selected_float_vars, columns=selected_object_vars,
+                                                     aggfunc='mean')
+                        cross_table.columns = cross_table.columns.map(' '.join)
+                        cross_table = cross_table.reset_index().rename(columns={'index': 'VariableName'})
+
+                        st.write("cross_table mit untervariablen in der Spalte")
+                        st.write(cross_table)
+                        """
+
+
+
+                        st.info("cross tables with average of categries")
+
+
+                        # Group by selected object variables and calculate average values for selected float variables
+
+                        #thomasTest
+
+                        ThomasFormatiertesDataframe = pd.DataFrame(columns=['KatVariable'] + list(selected_float_vars))
+                        AnzahlKategorischeVariablen = len(selected_categorical_cols)
+                        for t in range(AnzahlKategorischeVariablen):
+                            Thomasgrouped_df = merged_df.groupby(selected_object_vars[t])[selected_float_vars].mean().reset_index()
+                            Thomasgrouped_df.columns.values[0] = "KatVariable"
+                            #st.write("Thomasgrouped_df: ", Thomasgrouped_df)
+
+                            #ThomasFormatiertesDataframe.append(Thomasgrouped_df)
+                            ThomasFormatiertesDataframe = pd.concat([ThomasFormatiertesDataframe, Thomasgrouped_df])
+                            ThomasFormatiertesDataframe = ThomasFormatiertesDataframe.reset_index(drop=True)
+                            ThomasFormatiertesDataframe.index = ThomasFormatiertesDataframe['KatVariable']
+                            #ThomasFormatiertesDataframe = ThomasFormatiertesDataframe.set_index('KatVariable')
+                            #ThomasFormatiertesDataframe = ThomasFormatiertesDataframe.drop(['KatVariable'],axis = 1, inplace = True)
+                            TransposedDataframe = ThomasFormatiertesDataframe.T
+                            TransposedDataframe = TransposedDataframe.drop(TransposedDataframe.index[0])
+                            #ThomasFormatiertesDataframe = ThomasFormatiertesDataframe.transpose()
+
+
+                        st.write("Table with categries in the columns:  ", TransposedDataframe)
+
+                        def to_excel(TransposedDataframe):
+                            output = BytesIO()
+                            writer = pd.ExcelWriter(output, engine='xlsxwriter')
+                            TransposedDataframe.to_excel(writer, index=True, sheet_name='Sheet1')
+                            workbook = writer.book
+                            worksheet = writer.sheets['Sheet1']
+                            format1 = workbook.add_format({'num_format': '0.00'})
+                            worksheet.set_column('A:A', None, format1)
+                            writer.save()
+                            processed_data = output.getvalue()
+                            return processed_data
+
+
+                        df_xlsx = to_excel(TransposedDataframe)
+                        st.download_button(label='📥 Export Table with categories in the columns to Excel?',
+                                           data=df_xlsx,
+                               file_name='SPSSCrossTableToExcel.xlsx')
+
+                        st.write("")
+                        ThomasFormatiertesDataframe.drop(['KatVariable'], axis=1, inplace=True)
+                        st.write("Table with categories in the rows: ",ThomasFormatiertesDataframe)
+
+
+                        def to_excel(ThomasFormatiertesDataframe):
+                            output = BytesIO()
+                            writer = pd.ExcelWriter(output, engine='xlsxwriter')
+                            ThomasFormatiertesDataframe.to_excel(writer, index=True, sheet_name='Sheet1')
+                            workbook = writer.book
+                            worksheet = writer.sheets['Sheet1']
+                            format1 = workbook.add_format({'num_format': '0.00'})
+                            worksheet.set_column('A:A', None, format1)
+                            writer.save()
+                            processed_data = output.getvalue()
+                            return processed_data
+
+
+                        df_xlsx = to_excel(ThomasFormatiertesDataframe)
+                        st.download_button(label='📥 Export Table with categories in the rows to Excel?',
+                                           data=df_xlsx,
+                               file_name='SPSSCrossTTableRowCategoriesToExcel.xlsx')
+
+
+
+
+
+
+
+
+
+        ########################################## Metadata  ##############################################################################################################################
        
        
         st.write("")
+        st.write('---')
         st.write("")
 
         MetaDataExpander = st.expander("Show & save Meta-Data (Definition of Variables, Labels) ?") ############################
