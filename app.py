c5e150b210c454903fdc0fd6bd4019511d769e82
OMGToFo <93513240+OMGToFo@users.noreply.github.com>
2024-11-29 12:41:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,12 @@ st.set_page_config(page_title='SPSS Viewer',layout="wide")
 
 st.title("SPSS File Viewer")
 st.info("Here you can view SPSS-Files with/without labels and also export them to Excel and do some basic statistical testing and tabulation")
-st.warning("Unfortunatlely this app crashes kinda often, i think due to data usage limits")
+st.markdown('''
+Unfortunatlely this app crashes kinda often, due to data usage limits. 
+It's better to run this app locally on your computer. Follow these steps:
+Clone the repository to your local machine.
+Install the required dependencies by running the command pip install -r requirements.txt.
+Run the app by typing streamlit run app.py in your terminal''')
 
 col_names_labels_df = pd.DataFrame()
 
